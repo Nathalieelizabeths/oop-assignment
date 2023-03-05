701b3b998bbd301234bb85bea2382a5fa59001e2
Nathalieelizabeth <elizabethsnathalie@gmail.com>
2023-03-05 12:35:44 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    val nathalie=human("nathalie",20,50)
+    nathalie.eats(foodweight = 50)
+    var speech=("freedom of speech")
+    println(speech)
+    var age=nathalie.age++
+    println(nathalie.age++)
+var myuser=linet("linet","someone","someonelinet@gmail.com",710285415,44567)
+    println(myuser.email)
+    println(myuser.phonenumber)
+
+}
+class human( var name:String, var age:Int, var weightKg:Int){
+    fun eats(foodweight: Int){
+        println("I am eating $foodweight kgs of food")
+        fun speak(speech:String){
+            fun birthday(age:Int){
+
+            }
+
+        }
+        }
+
+    }
+data class linet(var firstname:String,var lastname:String,var email:String,var phonenumber:Int,var password:Int)
